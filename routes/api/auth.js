8f13fbaf0db33c3d05c9fba14c5c065187f56553
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-09-20 11:48:47 -0400

    register endpoint modified

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,7 @@ const {schemas} = require("../../models/user");
 const router = express.Router();
 
 // signup
-router.post("/signup", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 // signin
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
